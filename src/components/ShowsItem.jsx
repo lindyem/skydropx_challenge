e150f7a03cdc1573d3e7b44cf2d9f8fb6ad554ed
Lindy Merwin <lindymerwin@Lindys-MacBook-Air.local>
2021-01-31 11:13:18 -0600

    Render poster and average in ShowsItem

--- src/components/ShowsItem.jsx
+++ src/components/ShowsItem.jsx
@@ -5,6 +5,8 @@ function ShowsItem({item}) {
   return (
     <div>
       {item.name}
+      <div>{ item.vote_average }</div>
+      <div><img height="100px" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}?api_key=9187861de4b69a7a0899826a4bdf2f74`} alt="" /></div>
     </div>
   )
 }
