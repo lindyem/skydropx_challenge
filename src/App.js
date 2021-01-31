a8e9dabc1025e1ce3b87ea687d4bac5dffe1a497
Lindy Merwin <lindymerwin@Lindys-MacBook-Air.local>
2021-01-30 19:42:42 -0600

    inital commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MoviesList from "./components/MoviesList.jsx";
+
+function App() {
+  return (
+    <div >
+      <MoviesList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from React;
+import axios from axios; 
+
+function MoviesList() {
+  
+   useEffect(() => {
+     axios.get
+   }, [])
+  return (
+
+    <div>
+    </div>
+  )
+}
+
+export default MoviesList;
