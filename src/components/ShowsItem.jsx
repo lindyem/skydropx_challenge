0a27d62603025c93d4a470e94e0cbcc03adea39e
Lindy Merwin <lindymerwin@Lindys-MacBook-Air.local>
2021-01-31 17:04:48 -0600

    Modal with show information

--- src/components/ShowsItem.jsx
+++ src/components/ShowsItem.jsx
@@ -1,15 +1,37 @@
 import { useHistory, useParams } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import Modal from 'react-modal';
 import axios from "axios";
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root')
 
 function ShowsItem({ item }) {
   const [details, setDetails] = useState({});
   let history = useHistory();
   let { id } = useParams();
 
+  const [modalIsOpen,setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal(){
+    setIsOpen(false);
+  }
+
   const handleClick = () => {
     history.push(`/show/${item.id}`);
+    openModal();
   }
 
   useEffect(() => {
@@ -21,12 +43,26 @@ function ShowsItem({ item }) {
     }
   }, [id])
 
+
+
   return (
+    <Fragment>
     <div onClick={handleClick}>
       {item.name}
       <div>{ item.vote_average }</div>
       <div><img height="100px" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}?api_key=9187861de4b69a7a0899826a4bdf2f74`} alt="" /></div>
-      {+id === item.id && (
+    </div>
+    <Modal
+    isOpen={modalIsOpen}
+    onRequestClose={closeModal}
+    style={customStyles}
+    contentLabel="Example Modal">
+
+    <h2>Hello</h2>
+    <button onClick={closeModal}>close</button>
+    {item.name}
+      <div>{ item.vote_average }</div>
+      <div><img height="100px" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}?api_key=9187861de4b69a7a0899826a4bdf2f74`} alt="" /></div>
         <div>
           {details?.overview}
           Genre: {details?.genres?.map((genre) => {
@@ -34,8 +70,8 @@ function ShowsItem({ item }) {
           })}
          Run Time: {details?.episode_run_time && details.episode_run_time[0]}
         </div>
-      )}
-    </div>
+      </Modal>
+    </Fragment>
   )
 }
 
