1cd212aa39f9bb4534fe100f60ced356ae58293c
Lindy Merwin <lindymerwin@Lindys-MacBook-Air.local>
2021-02-02 19:30:34 -0600

    Sort functionality

---
+++ src/components/FilterSelect/index.jsx
@@ -0,0 +1,19 @@
+function FilterSelect({ onFilter, name }) {
+  
+  const handleFilter = (e) => {
+    onFilter(e.target.value, name);
+  }
+
+  return (
+    <div>
+      <select name="select" onChange={handleFilter}>
+        <option value="" disabled selected>Sort By</option>
+        <option value="alphabet">A-Z</option>
+        <option value="rating">Rating</option>
+      </select>
+    </div>
+  )
+}
+
+
+export default FilterSelect
--- src/components/ShowList/index.jsx
+++ src/components/ShowList/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import ShowsItem from '../ShowItem';
+import FilterSelect from '../FilterSelect';
 
 import  './ShowList.css';
 
@@ -24,44 +25,78 @@ function ShowsList() {
     axios.get('https://api.themoviedb.org/3/tv/on_the_air?api_key=9187861de4b69a7a0899826a4bdf2f74')
       .then((response) => {
         setOnTheAir(response.data.results);
-    })
+      })
+    
+    
   }, [])
+
+  const handleOnFilter = (filterValue, name) => {
+    if (name === 'popular') {
+      if (filterValue === 'alphabet') {
+        let filteredArray = [...popular].sort((a, b) => (a.name > b.name) ? 1 : -1);
+        setPopular(filteredArray)
+      } else {
+        let filteredArray = [...popular].sort((a, b) => (a.vote_average < b.vote_average) ? 1 : -1);
+        setPopular(filteredArray)
+      }
+    } else if (name === 'top') {
+      if (filterValue === 'alphabet') {
+        let filteredArray = [...top].sort((a, b) => (a.name > b.name) ? 1 : -1);
+        setTop(filteredArray)
+      } else {
+        let filteredArray = [...top].sort((a, b) => (a.vote_average < b.vote_average) ? 1 : -1);
+        setTop(filteredArray)
+      }
+    } else if (name === 'onTheAir') {
+      if (filterValue === 'alphabet') {
+        let filteredArray = [...onTheAir].sort((a, b) => (a.name > b.name) ? 1 : -1);
+        setOnTheAir(filteredArray)
+      } else {
+        let filteredArray = [...onTheAir].sort((a, b) => (a.vote_average < b.vote_average) ? 1 : -1);
+        setOnTheAir(filteredArray)
+      }
+    } else if (name === 'favorite') {
+      if (filterValue === 'alphabet') {
+        let filteredArray = [...popular].sort((a, b) => (a.name > b.name) ? 1 : -1);
+        //setPopular(filteredArray)
+      } else {
+        let filteredArray = [...popular].sort((a, b) => (a.vote_average < b.vote_average) ? 1 : -1);
+        //setPopular(filteredArray)
+      }
+    }
+  }
   
   
   return (
     <section>
-      <h1 className="headingCol">Popular <hr/></h1>
+      <h1 className="headingCol">Popular <FilterSelect name="popular" onFilter={handleOnFilter}/><hr/></h1>
       <div className="showRow">
-      {popular.map((element) => {
+        {popular.map((element) => {
           return (
             <ShowsItem item={element}/>
           )
         }) } 
       </div>
 
-      <h1 className="headingCol">Top Rated <hr/></h1>
+      <h1 className="headingCol">Top Rated <FilterSelect name="top" onFilter={handleOnFilter}/> <hr/></h1>
       <div className="showRow">
-      {top.map((element) => {
+        {top.map((element) => {
           return (
             <ShowsItem item={element}/>
           )
         }) }
       </div>
-      <h1 className="headingCol">Trending Now <hr/></h1>
+      <h1 className="headingCol">Trending Now <FilterSelect name="onTheAir" onFilter={handleOnFilter}/><hr/></h1>
       <div className="showRow">
-      {onTheAir.map((element) => {
+        {onTheAir.map((element) => {
           return (
             <ShowsItem item={element}/>
           )
         }) }
       </div>  
-      <h1 className="headingCol">Favorites <hr/></h1>
+      <h1 className="headingCol">Favorites <FilterSelect name="favorite" onFilter={handleOnFilter}/> <hr/></h1>
       <div className="showRow">
-      {onTheAir.map((element) => {
-          return (
-            <ShowsItem item={element}/>
-          )
-        }) }
+
       </div> 
     </section>
   )
